1060d395e874b44c1a60b0984ed9078a7f0ce745
Bryan Irace <bryan.irace@gmail.com>
2014-03-04 20:13:10 -0500

    Comments

--- BRYHashCodeBuilder/BRYHashCodeBuilder.h
+++ BRYHashCodeBuilder/BRYHashCodeBuilder.h
@@ -9,11 +9,28 @@
 @import Foundation;
 @import CoreGraphics;
 
-// https://commons.apache.org/proper/commons-lang/javadocs/api-3.1/org/apache/commons/lang3/builder/HashCodeBuilder.html
+/**
+ Utility for determining object hash codes, based on Apache Commons [HashCodeBuilder](https://commons.apache.org/proper/commons-lang/javadocs/api-3.1/org/apache/commons/lang3/builder/HashCodeBuilder.html). Useful for implemeting `-[NSObject hash]` methods.
+ 
+ - (NSUInteger)hash {
+     return [[[[[BRYHashCodeBuilder builderWithInitialValue:17 multiplier:37]
+                appendObject:self.name]
+               appendBool:self.isHidden]
+              appendFloat:self.width]
+             appendInteger:self.index].hashCode;
+ }
+ */
 @interface BRYHashCodeBuilder : NSObject
 
 @property (nonatomic, readonly) NSUInteger hashCode;
 
+/**
+ Factory method for acquiring a `BRYHashCodeBuilder` instance.
+ 
+ @param initial Randomly chosen, non-zero, odd number. Ideally different for each class, however this is not vital. Preferably prime.
+ @param multiplier Randomly chosen, non-zero, odd number. Ideally different for each class, however this is not vital. Preferably prime.
+ @return `BRYHashCodeBuilder` instance seeded with the initial value and multiplier
+ */
 + (instancetype)builderWithInitialValue:(NSUInteger)initial multiplier:(NSUInteger)multiplier;
 
 - (BRYHashCodeBuilder *)appendObject:(id)object;
